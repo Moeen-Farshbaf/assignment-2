8c004282373168c7d89e5f6bb37d4129899c2a0f
Moeen-Farshbaf <88230820+Moeen-Farshbaf@users.noreply.github.com>
2021-08-04 14:55:57 +0430

    Updated

--- studentsscores.py
+++ studentsscores.py
@@ -1,14 +1,15 @@
 studscount = int(input("How many students you got?"))
 studsgrads = []
 for i in range(studscount):
-    grade = input("Enter their grades when you were done enter done:")
+    grade = float(input("Enter their grades when you were done enter done:"))
     studsgrads.append(grade)
     studsgrads.sort()
 if len(studsgrads) == studscount:
-        
+    score_average = sum(studsgrads)/studscount
+    print ("Average score is: "+str(score_average))
     print ("Highest score was:"+str(studsgrads[studscount-1]))
     print ("Lowest score was:"+str(studsgrads[0]))
     print (studsgrads)
     print ('Here is a list of grades:')
     for grade in studsgrads:
-            print(grade + '\n')
+        print(str(grade) + '\n')
